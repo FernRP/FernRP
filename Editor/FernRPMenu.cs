63d4bb4ba17f746aa7ea1304949b843efc522a0f
Dejhon <itact@hotmail.com>
2024-04-24 08:58:18 +0800

    add raytracingvolume editor; add EnableDeveloperMode Menu

---
+++ Editor/FernRPMenu.cs
@@ -0,0 +1,17 @@
+﻿namespace UnityEditor.Rendering.Universal.Internal
+{
+    public class FernRPMenu
+    {
+        [MenuItem("Fern RP/DeveloperMode/Enable")]
+        private static void EnableDeveloperMode()
+        {
+            EditorPrefs.SetBool("DeveloperMode", true);
+        }
+        
+        [MenuItem("Fern RP/DeveloperMode/Disable")]
+        private static void DisableeveloperMode()
+        {
+            EditorPrefs.SetBool("DeveloperMode", false);
+        }
+    }
+}
---
+++ Editor/Overrides/RayTracingVolumeEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace UnityEditor.Rendering.Universal
+{
+    
+    [CustomEditor(typeof(RayTracingVolume))]
+    public class RayTracingVolumeEditor : VolumeComponentEditor
+    {
+        public override void OnInspectorGUI()
+        {
+            using (new EditorGUI.DisabledGroupScope(serializedObject.isEditingMultipleObjects))
+            {
+                if (GUILayout.Button("Setup RayTracing Data", EditorStyles.miniButton))
+                {
+                    // TODO: Start building BVH 
+                }
+            }
+        }
+    }
+}
